de07328519220c3bbb0bd297bae0c479c3a2fc74
kjm6696 <kjm6696@psu.edu>
2023-09-24 22:25:18 -0400

    Album Class Created for Linked List

--- src/main/java/Album.java
+++ src/main/java/Album.java
@@ -1,2 +1,18 @@
 public class Album {
+    int artistID;
+    String artistName;
+    String albumName;
+    int albumSongs;
+
+    public Album(int artistID, String artistName, String albumName, int albumSongs) {
+        this.artistID = artistID;
+        this.artistName = artistName;
+        this.albumName = albumName;
+        this.albumSongs = albumSongs;
+    }
+
+    @Override
+    public String toString() {
+        return artistID + ": " + albumSongs + " -- [" + artistName + "]";
+    }
 }
